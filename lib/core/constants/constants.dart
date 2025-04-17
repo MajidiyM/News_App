b9bf2eebad86a0e0d20182bb344362bc3c1938a6
Mirislom <m.mirislom.m@gmail.com>
2025-04-17 19:59:53 +0500

    [Edit] article_repository_impl.dart and constants.dart [+] news_api_service.g.dart

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,1 +1,4 @@
 const String newsAPIBaseURL = "https://newsapi.org/v2";
+const String newsAPIKey = "aa669e41a1b04698881b53445f2210ee";
+const String countryQuery = "us";
+const String categoryQuery = "general";
