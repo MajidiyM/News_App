0b32200eb6042a5234ba13eef4b4350259b008fb
Mirislom <m.mirislom.m@gmail.com>
2025-04-17 19:06:08 +0500

    [+] Article Model

---
+++ lib/features/daily_news/data/models/article.dart
@@ -0,0 +1,27 @@
+import 'package:news_app/features/daily_news/domain/entities/article.dart';
+
+class ArticleModel extends ArticleEntity {
+  ArticleModel(
+    super.id,
+    super.author,
+    super.title,
+    super.description,
+    super.url,
+    super.urlToImage,
+    super.publishedAt,
+    super.content,
+  );
+
+  factory ArticleModel.fromJson(Map<String, dynamic> map) {
+    return ArticleModel(
+      map['id'] ?? "",
+      map['author'] ?? "",
+      map['title'] ?? "",
+      map['description'] ?? "",
+      map['url'] ?? "",
+      map['urlToImage'] ?? "",
+      map['publishedAt'] ?? "",
+      map['content'] ?? "",
+    );
+  }
+}
